94c933b9e29c8f6983585ebe921cca480c5c0522
Shinmera <shinmera@tymoon.eu>
2016-09-06 16:05:26 +0200

    Cleanup, renames, make it compile.

--- ssa.lisp
+++ ssa.lisp
@@ -4,65 +4,30 @@
  Author: Nicolas Hafner <shinmera@tymoon.eu>
 |#
 
+;;; IR1
 (in-package #:org.shirakumo.flare.vector)
 
 (sb-c:defknown %rsqrtss ((sb-ext:simd-pack single-float)) (sb-ext:simd-pack single-float)
     (sb-c:foldable sb-c:flushable sb-c:movable)
   :overwrite-fndb-silently T)
 
-(defun %rsqrtss (x)
-  (%rsqrtss x))
-
-(declaim (inline rsqrtss))
-(declaim (ftype (function (single-float) single-float) rsqrtss))
-(defun rsqrtss (x)
-  (let ((pack (%rsqrtss (sb-ext:%make-simd-pack-single x 0.0s0 0.0s0 0.0s0))))
-    (sb-c::%primitive sb-vm::%simd-pack-single-item pack 0)))
-
 (sb-c:defknown %rsqrtps ((sb-ext:simd-pack single-float)) (sb-ext:simd-pack single-float)
     (sb-c:foldable sb-c:flushable sb-c:movable)
   :overwrite-fndb-silently T)
 
-(defun %rsqrtps (x)
-  (%rsqrtps x))
-
-(sb-c:defknown %f4* ((sb-ext:simd-pack single-float)
+(sb-c:defknown %mulps ((sb-ext:simd-pack single-float)
                      (sb-ext:simd-pack single-float)) (sb-ext:simd-pack single-float)
     (sb-c:foldable sb-c:flushable sb-c:movable sb-c:always-translatable)
   :overwrite-fndb-silently T)
 
-(defun %f4* (x y)
-  (%f4* x y))
-
-(declaim (inline f4*))
-(declaim (ftype (function (single-float single-float single-float single-float single-float single-float single-float single-float)
-                          (values single-float single-float single-float single-float)) f4*))
-(defun f4* (a b c d x y z w)
-  (sb-ext:%simd-pack-singles (%f4* (sb-ext:%make-simd-pack-single a b c d)
-                                   (sb-ext:%make-simd-pack-single x y z w))))
-
-(sb-c:defknown %f4/+ ((sb-ext:simd-pack single-float)
+(sb-c:defknown %haddps ((sb-ext:simd-pack single-float)
                       (sb-ext:simd-pack single-float)) (sb-ext:simd-pack single-float)
     (sb-c:foldable sb-c:flushable sb-c:movable sb-c:always-translatable)
   :overwrite-fndb-silently T)
 
-(defun %f4/+ (x y)
-  (%f4/+ x y))
-
-(defun f4/+ (a b c d)
-  (let* ((pack (sb-ext:%make-simd-pack-single a b c d))
-         (pack (%f4/+ pack pack))
-         (pack (%f4/+ pack pack)))
-    (sb-c::%primitive sb-vm::%simd-pack-single-item pack 0)))
-
-(defun simd-vunit (x y z w)
-  (let* ((pk (sb-ext:%make-simd-pack-single x y z w))
-         (sq (%f4* pk pk))
-         (su (%f4/+ sq sq))
-         (su (%f4/+ su su)))
-    (%f4* pk (%rsqrtps su))))
-
+;;; IR2
 (in-package #:sb-vm)
+
 (define-vop (3d-vectors::%rsqrtss)
   (:policy :fast-safe)
   (:translate 3d-vectors::%rsqrtss)
@@ -83,9 +48,9 @@
   (:generator 3
               (inst rsqrtps r x)))
 
-(define-vop (3d-vectors::%f4*)
+(define-vop (3d-vectors::%mulps)
   (:policy :fast-safe)
-  (:translate 3d-vectors::%f4*)
+  (:translate 3d-vectors::%mulps)
   (:args (x :scs (single-sse-reg) :target r)
          (y :scs (single-sse-reg)))
   (:arg-types simd-pack-single simd-pack-single)
@@ -98,9 +63,9 @@
                      (move r x)
                      (inst mulps r y)))))
 
-(define-vop (3d-vectors::%f4/+)
+(define-vop (3d-vectors::%haddps)
   (:policy :fast-safe)
-  (:translate 3d-vectors::%f4/+)
+  (:translate 3d-vectors::%haddps)
   (:args (x :scs (single-sse-reg) :target r)
          (y :scs (single-sse-reg)))
   (:arg-types simd-pack-single simd-pack-single)
@@ -112,3 +77,38 @@
                     (T
                      (move r x)
                      (inst haddps r y)))))
+
+;;; High-level
+(in-package #:org.shirakumo.flare.vector)
+
+(macrolet ((stubdef (name args) `(defun ,name ,args (,name ,@args))))
+  (stubdef %rsqrtss (x))
+  (stubdef %rsqrtps (x))
+  (stubdef %mulps (x y))
+  (stubdef %haddps (x y)))
+
+(declaim (inline rsqrtss))
+(declaim (ftype (function (single-float) single-float) rsqrtss))
+(defun isqrt~ (x)
+  (nth-value 0 (sb-ext:%simd-pack-singles
+                (%rsqrtss (sb-ext:%make-simd-pack-single x 0.0s0 0.0s0 0.0s0)))))
+
+(declaim (inline f4*))
+(declaim (ftype (function (single-float single-float single-float single-float single-float single-float single-float single-float)
+                          (values single-float single-float single-float single-float)) f4*))
+(defun f4* (a b c d x y z w)
+  (sb-ext:%simd-pack-singles (%mulps (sb-ext:%make-simd-pack-single a b c d)
+                                   (sb-ext:%make-simd-pack-single x y z w))))
+
+(defun f4/+ (a b c d)
+  (let* ((pack (sb-ext:%make-simd-pack-single a b c d))
+         (pack (%haddps pack pack))
+         (pack (%haddps pack pack)))
+    (nth-value 0 (sb-ext:%simd-pack-singles pack))))
+
+(defun simd-vunit (x y z w)
+  (let* ((pk (sb-ext:%make-simd-pack-single x y z w))
+         (sq (%mulps pk pk))
+         (su (%haddps sq sq))
+         (su (%haddps su su)))
+    (%mulps pk (%rsqrtps su))))
