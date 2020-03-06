5503d92d7d3dd3285d47320bb18b09fc2fca0c17
Shinmera <shinmera@tymoon.eu>
2020-03-06 17:59:44 +0100

    Some thoughts.

--- ops.lisp
+++ ops.lisp
@@ -9,6 +9,13 @@
 ;; FIXME: Need to be able to specify template arguments for the operation only,
 ;;        not for the type being templated, so that we can re-use the template
 ;;        for + - * / etc.
+;;
+;;        It occurs to me that we should always inherit the type's template
+;;        parameters, so we only have to specify new ones here.
+;;        Another thought is that we should be able to template on each argument
+;;        type if so desired.
+;;        That kind of information could also be saved somewhere else so that the
+;;        dispatch generation could be automated more easily.
 (define-template 2vec+ vec <s> <t> (a b)
   `(declare (type ,name a b))
   `(,constructor
